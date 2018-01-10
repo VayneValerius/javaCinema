e61f203142ee3c9d8ae8b78be35236bd451ba857
VayneValerius <joshuakingswell@gmail.com>
2018-01-10 10:26:51 +0000

    jsonOnMain

--- cinemaApplication/src/main/java/jKingswell/cinemaApplication/Main.java
+++ cinemaApplication/src/main/java/jKingswell/cinemaApplication/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Service cinemaService = new Service();
+		jsonConversion convert = new jsonConversion();
 		
 		Movie movie1 = new Movie("Harry Putin and the Goblet of Atomic Fire", "Comedy", "PG");
 		Movie movie2 = new Movie("Star Wars Episode Disney", "Comedy", "PG");
@@ -13,6 +14,8 @@ public class Main {
 		cinemaService.showMovies();
 		cinemaService.getMovie(1);
 		cinemaService.removeMovie(0);
+		
+		convert.convertHashMap(cinemaService.movieList);
 	}
 
 }
