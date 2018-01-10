536a3363f198ccd9a778bfa3f26ef74d0da0a3ac
VayneValerius <joshuakingswell@gmail.com>
2018-01-10 10:06:43 +0000

    Commit

---
+++ cinemaApplication/src/main/java/jKingswell/cinemaApplication/jsonConversion.java
@@ -0,0 +1,16 @@
+package jKingswell.cinemaApplication;
+
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
+public class jsonConversion {
+	JSONObject cinemaJSON = new JSONObject();
+	
+	public void convertHashMap(HashMap hm) {
+		//cinemaJSON.put("movie", movie);
+		cinemaJSON.put("movie", hm);
+		System.out.print(cinemaJSON);
+	}
+
+}
